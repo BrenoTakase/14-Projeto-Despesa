ac7c61852abddd5532d6e845440431d07d275ba7
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-21 11:40:27 -0300

    Enviando projeto atualizado

--- src/main/java/br/fiap/conexao/TesteDespesa.java
+++ src/main/java/br/fiap/conexao/TesteDespesa.java
@@ -1,4 +1,37 @@
 package br.fiap.conexao;
 
+import br.fiap.modelo.Categoria;
+import br.fiap.modelo.Despesa;
+import br.fiap.modelo.DespesaDAO;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 public class TesteDespesa {
+    public static void main(String[] args) {
+
+        //Traz os objetos de CategoriaDAO
+        DespesaDAO dao = new DespesaDAO();
+        //Parecido com o DF, mas para data
+        DateTimeFormatter mascara = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+//        Despesa despesa = new Despesa();
+//        despesa.setId(103L);
+//        despesa.setDescricao("Monster para assistir na aula");
+//        despesa.setValor(78.00);
+//        despesa.setData(LocalDate.parse("21/08/2025", mascara));
+//        despesa.setCategoria(new Categoria(3L, ""));
+//        dao.inserir(despesa); //Ctrl + / coloca tudo selecionado em comentário
+
+        List<Despesa> lista = dao.listar();
+        for (Despesa despesa: lista){
+            System.out.println(despesa.getId());
+            System.out.println(despesa.getDescricao());
+            System.out.println(despesa.getValor());
+            System.out.println(despesa.getData());
+            System.out.println(despesa.getCategoria().getId());
+            System.out.println("=============================");
+        }
+    }
 }
--- src/main/java/br/fiap/modelo/DespesaDAO.java
+++ src/main/java/br/fiap/modelo/DespesaDAO.java
@@ -1,4 +1,58 @@
 package br.fiap.modelo;
 
+import br.fiap.conexao.Conexao;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DespesaDAO {
+    //ATRIBUTOS
+    // Ela é sabe onde está o banco de dados, como chegar até o banco
+    // e transporta nossos comandos para dentro do banco
+    private PreparedStatement ps;
+    //Mostra o resultado das nossas inserções dentro do SQL
+    private ResultSet rs;
+    private String sql;
+
+    //METODOS
+    public void inserir(Despesa despesa){
+        sql = "insert into java_despesa values (?, ?, ?, ?, ?)";
+
+        try(Connection connection = Conexao.conectar()) {
+            ps = connection.prepareStatement(sql);
+            //Abaixo configuramos cada uma das "?" com seu determinado elemento
+            ps.setLong(1, despesa.getId());
+            ps.setString(2, despesa.getDescricao());
+            ps.setDouble(3, despesa.getValor());
+            ps.setDate(4, Date.valueOf(despesa.getData()));
+            ps.setLong(5, despesa.getCategoria().getId());
+            ps.execute();
+        }
+        catch (SQLException e){
+            System.out.println("Erro ao inserir despesa\n" + e);
+        }
+    }
+
+    public List<Despesa> listar(){
+        List<Despesa> lista = new ArrayList<>();
+        sql = "select * from java_despesa";
+        try (Connection connection = Conexao.conectar()){
+            ps = connection.prepareStatement(sql);
+            rs = ps.executeQuery();
+           while(rs.next()){
+               Despesa despesa = new Despesa();
+               despesa.setId(rs.getLong("id_despesa"));
+               despesa.setDescricao(rs.getString("descricao"));
+               despesa.setValor(rs.getDouble("valor"));
+               despesa.setData(rs.getDate("data").toLocalDate());
+               despesa.setCategoria(new Categoria(rs.getLong("id_categoria"), ""));
+               lista.add(despesa);
+           }
+        }
+        catch (SQLException e){
+            System.out.println("Erro ao listar despesas\n" + e);
+        }
+        return lista;
+    }
 }
